1dea118d220c81672d2e64d08f07d6c3cf356b48
Kaushik Kundu <kaushikkundu774@gmail.com>
2023-09-10 23:16:48 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,96 @@
-import React, { useRef } from 'react';
-import { useState } from 'react'
-import FormInput from './FormInput';
-import './App.css';
+import React, { useRef } from "react";
+import { useState } from "react";
+import FormInput from "./FormInput";
+import "./App.css";
 
 function App() {
-  const [username, setusername] = useState("");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  }); //input types
   const inputs = [
     {
-      id:1,
-      name:'Username',
-      type:'text',
-      placeholder:"Username",
-      label:"Username"
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+      errorMessage:"Usernames should be 3-16 characters and shouldn't contain special character and spaces",
+      pattern:"^[A-Za-z0-9]{3,16}$",
+      required:true
     },
     {
-      id:2,
-      name:'Mobile No',
-      type:'number',
-      placeholder:"9102049292",
-      label:"Mobile No"
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+      errorMessage:"Must be an email address",
+      required:true
     },
     {
-      id:3,
-      name:'Birthday',
-      type:'date',
-      placeholder:"Birthday",
-      label:"Birthday"
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+      errorMessage:"Must be a valid date",
+      required:false
     },
     {
-      id:4,
-      name:'Email',
-      type:'text',
-      placeholder:"Email",
-      label:"Email"
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+      pattern:"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,20}$"
+      ,
+      errorMessage:"Password should be 8-20 characters and include at least 8 letter,1 number and a special character",
+      required:true
     },
     {
-      id:5,
-      name:'Confirm Password',
-      type:'password',
-      placeholder:"Confirm Password",
-      label:"Confirm Password"
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password",
+      pattern: values.password,
+      errorMessage:"Passwords don't match!",
+      required:true
     },
-  ]
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
 
+  function onChange(e) {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   }
+
   return (
     <div className="app">
+      <h3 id="register">Register</h3>
       <form onSubmit={handleSubmit}>
-        <FormInput placeholder={username} label="Username" onChange={(e) => setusername(e.target.value)}/>
-        <FormInput placeholder={username} label="Mobile No"/>
-        <FormInput placeholder={username}/>
-        <FormInput placeholder={username}/>
-       <button>Submit</button>
-       
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
       </form>
+      <button>Submit</button>
     </div>
-  )
-  
+  );
 }
 
 export default App;
--- src/FormInput.jsx
+++ src/FormInput.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import './FormInput.css'
+import React, { useState } from "react";
+import "./FormInput.css";
 
-const formInput = (props) => {
-    return ( 
-        <div className="form">
-            <label >{props.label}</label>
-            <input type="text" placeholder={props.placeholder} />
-           
-        </div>
-     );
-}
- 
-export default formInput;
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, ...inputProps } = props;
+
+  function handleFocus() {
+    setFocused(true);
+  }
+  
+  return (
+    <div className="formInput">
+      <label>
+        {label}
+        </label>
+        <input
+          onChange={onChange}
+          {...inputProps}
+          onFocus={handleFocus}
+          focused={focused.toString()}
+        />
+        <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
